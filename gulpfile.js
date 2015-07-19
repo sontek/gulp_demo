e2bc1b9ead2c1b5c7f387362c68f637d73e2e7b8
John Anderson <sontek@gmail.com>
2015-07-19 15:40:56 -0700

    Got it all working!

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,46 @@
-'use strict';
-
-var babel = require('babelify');
-var browserify = require('browserify');
-var buffer = require('vinyl-buffer');
-var del = require('del');
-var gulp = require('gulp')
-var source = require('vinyl-source-stream');
-var sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const browserify = require('browserify');
+const buffer = require('vinyl-buffer');
+const del = require('del');
+const gulp = require('gulp')
+const source = require('vinyl-source-stream');
+const sourcemaps = require('gulp-sourcemaps');
 
 
 var paths = {
-    scripts: ['*.jsx']
+    scripts: ['src/*.jsx']
 }
 
 gulp.task('clean', function(cb) {
-    del(['build'], cb);
+    del(['./lib', './dist'], cb);
 });
 
-gulp.task('scripts', ['clean'], function() {
-    // Minify and copy all JS scripts with sourcemaps
-    var bundler = browserify(
-        'index.jsx', {debug: true}
-    ).transform(babel.configure({}));
+// Translate from es6 to pure javascript
+gulp.task('babel', function() {
+    return gulp.src(paths.scripts)
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(babel())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./lib'));
+});
 
-    return bundler.bundle()
+gulp.task('browserify', ['clean', 'babel'], function() {
+    // Minify and copy all JS scripts with sourcemaps
+    return browserify('lib/index.js', {debug: true})
+        .bundle()
         .pipe(source('bundle.js'))
         .pipe(buffer())
 
         // start tracking sourcemaps
         .pipe(sourcemaps.init({loadMaps: true}))
+
         // write out the sourcemaps
         .pipe(sourcemaps.write('.'))
 
         // write out the build
-        .pipe(gulp.dest('build/'))
+        .pipe(gulp.dest('./dist'))
 
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['scripts']);
+gulp.task('default', ['browserify']);
