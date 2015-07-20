0fccfbb602e023a86bb35c9d4699a6b3754e427a
John Anderson <sontek@gmail.com>
2015-07-19 19:05:58 -0700

    finalize babelify port

--- gulpfile.js
+++ gulpfile.js
@@ -15,26 +15,20 @@ gulp.task('clean', function(cb) {
     del(['./lib', './dist'], cb);
 });
 
-// Translate from es6 to pure javascript
-gulp.task('babel', ['clean'], function() {
-    return gulp.src(paths.scripts)
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(babel())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./lib'));
-});
-
 gulp.task('browserify', ['clean'], function() {
     // Minify and copy all JS scripts with sourcemaps
-    return browserify('src/index.jsx', {debug: true})
-        .transform(babelify)
-        .bundle()
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(source('bundle.js'))
-        .pipe(sourcemaps.write('.'))
+    return browserify({
+        entries: 'src/index.jsx',
+        extensions: ['.js', '.jsx'],
+        debug: true
+    })
 
-        // write out the build
-        .pipe(gulp.dest('./dist'))
+   .transform(babelify)
+   .bundle()
+   .pipe(source('bundle.js'))
+
+    // write out the build
+    .pipe(gulp.dest('./dist'))
 
 });
 
