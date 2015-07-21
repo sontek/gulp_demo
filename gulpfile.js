a0cf68076625afc97cf94390640c4d5392014f0b
John Anderson <sontek@gmail.com>
2015-07-20 21:38:41 -0700

    Add exorcist for extracting sourcemap

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,40 @@
-const babelify = require('babelify');
-const browserify = require('browserify');
-const del = require('del');
-const gulp = require('gulp')
-const source = require('vinyl-source-stream');
-const sourcemaps = require('gulp-sourcemaps');
+const babelify   = require('babelify')
+    , browserify = require('browserify')
+    , del        = require('del')
+    , fs         = require('fs')
+    , gulp       = require('gulp')
+    , path       = require('path')
+    , source     = require('vinyl-source-stream')
+    , sourcemaps = require('gulp-sourcemaps')
+    , exorcist = require('exorcist');
+
+const dist_dir = path.join(__dirname, 'dist/')
+    , map_file = path.join(dist_dir, 'bundle.js.map');
 
 gulp.task('clean', function(cb) {
     del(['./lib', './dist'], cb);
 });
 
 gulp.task('browserify', ['clean'], function() {
-    // Minify and copy all JS scripts with sourcemaps
+    // exorcist doesn't write the file if the directory
+    // doesn't already exist.
+    if (!fs.existsSync(dist_dir)){
+        fs.mkdirSync(dist_dir);
+    }
+
     return browserify({
-        entries: 'src/index.jsx',
-        extensions: ['.js', '.jsx'],
-        debug: true
+        entries: 'src/index.jsx'
+        , extensions: ['.js', '.jsx']
+        , debug: true  // We use debug: true so source maps are generated
     })
 
    .transform(babelify)
    .bundle()
+   .pipe(exorcist(map_file))
    .pipe(source('bundle.js'))
 
-    // write out the build
-    .pipe(gulp.dest('./dist'))
+   // write out the build
+   .pipe(gulp.dest('./dist'))
 
 });
 
