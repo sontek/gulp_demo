af15929f9e21c8f2c2a6b3f7c8a2ab9218174f8f
BenL <benl@surveymonkey.com>
2015-08-07 10:59:51 -0700

    added karma test runner and simple tests for hello-world.jsx, run npm install and then test.sh to use

---
+++ src/__tests__/hello-world-test.jsx
@@ -0,0 +1,15 @@
+//jest.dontMock('../MainDiv.jsx');
+describe('HelloWorld', function() {
+  it('does stuff', function() {
+    var React = require('react/addons');
+    var HelloWorld = require('../hello-world.jsx');
+    var TestUtils = React.addons.TestUtils;
+
+    var main = TestUtils.renderIntoDocument(
+      <HelloWorld />
+      //React.createElement(HelloWorld)
+    );
+    var div = TestUtils.findRenderedDOMComponentWithTag(main, 'div');
+    expect(React.findDOMNode(main).textContent).toEqual('Hello, world!');
+  });
+});
